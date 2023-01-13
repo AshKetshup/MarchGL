5a45453601349b4ebfb49fafdb2135d072cc91b3
Diogo Simões <dsimoes2000@gmail.com>
2023-01-13 16:01:34 +0000

    Project Basis

---
+++ MarchGL/MarchGL/src/main.cpp
@@ -0,0 +1,44 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+int main(void) {
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "MarchGL | A Marching Cubes IsoSurfaces renderer", NULL, NULL);
+	if (!window) {
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	/* Initialize GLEW */
+	if (glewInit() != GLEW_OK)
+		std::cout << "Error initializing GLEW." << std::endl;
+
+	std::cout << "OpenGL Version " << glGetString(GL_VERSION) << std::endl;
+
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window)) {
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
